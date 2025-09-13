24ed45e7a5a14db3190071731baccb8c9571ff20
Pablo Cordero <cordero080@gmail.com>
2025-09-12 22:15:29 -0400

    chore: bump css cache-bust to 2.1

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.locals.owner = "Pablo C.";
 app.locals.startYear = 2024;
 app.locals.year = new Date().getFullYear();
 // 🔁 Cache-buster for static links in head.ejs, e.g. /css/main.css?v=<%= cssVersion %>
-app.locals.cssVersion = "2.0"; // bump this when you change any CSS
+app.locals.cssVersion = "2.1"; // bump this when you change any CSS
 
 // -------------- SECURITY & PERFORMANCE (MIDDLEWARE) --------------
 
